9975013af65e12b8c558eae5f89d9297c404ec32
Jovan Pavlovic <jovan@runamedia.com>
2018-09-18 14:55:51 +0200

    -improve button visibility, add the textures to the wall and floor objects

---
+++ ClientUnity/Unity/PeerCCUnity/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    public float sensitivity = 10f;
+    public float maxYAngle = 80f;
+    private Vector2 currentRotation;
+    private bool IsRightClick = false;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            IsRightClick = true;
+        }
+        else if (Input.GetMouseButtonUp(1))
+        {
+            IsRightClick = false;
+        }
+    }
+    private void LateUpdate()
+    {
+        if (IsRightClick)
+        {
+            currentRotation.x += Input.GetAxis("Mouse X") * sensitivity;
+            currentRotation.y -= Input.GetAxis("Mouse Y") * sensitivity;
+            currentRotation.x = Mathf.Repeat(currentRotation.x, 360);
+            currentRotation.y = Mathf.Clamp(currentRotation.y, -maxYAngle, maxYAngle);
+            transform.rotation = Quaternion.Euler(currentRotation.y, currentRotation.x, 0);
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+        
+    }
+}
